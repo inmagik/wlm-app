f2bb4e935a67eeef959ad014f3cff6f86362c4f8
skaffo <matteo.scaffidi@gmail.com>
2023-07-10 11:30:29 +0200

    Preparing documents list

--- src/pages/Mobile/List/List.tsx
+++ src/pages/Mobile/List/List.tsx
@@ -1,8 +1,10 @@
 import { Fragment } from 'react'
 import { Waypoint } from 'react-waypoint'
 import Layout from '../../../components/Mobile/Layout'
-import { useInfiniteMomuments, useMonuments } from '../../../hooks/monuments'
+import { useInfiniteMomuments } from '../../../hooks/monuments'
 import styles from './List.module.css'
+import { ReactComponent as CastelloIcon } from '../../../assets/castelli.svg'
+import { ReactComponent as Camera } from '../../../assets/camera.svg'
 
 export default function List() {
   const {
@@ -20,9 +22,29 @@ export default function List() {
         <div className={styles.ListMonuments}>
           {infiniteMonuments!.pages.map((list, i) => (
             <Fragment key={i}>
-              {list.results.map((monument, i) => {
+              {list.results.map((monument) => {
                 return (
-                  <div className={styles.MonumentCard} key={monument.id}></div>
+                  <div className={styles.MonumentCard} key={monument.id}>
+                    <div className="d-flex">
+                      <div>
+                        <CastelloIcon />
+                      </div>
+                      <div className="ms-2">
+                        <div className={styles.MonumentTitle}>
+                          {monument.label}
+                        </div>
+                        <div className={styles.City}>
+                          {monument.municipality_label}
+                        </div>
+                      </div>
+                    </div>
+                    <div>
+                      <div className={styles.NumberPhoto}>
+                        <div>{monument.pictures_count}</div>
+                        <Camera className="ms-2" />
+                      </div>
+                    </div>
+                  </div>
                 )
               })}
             </Fragment>
