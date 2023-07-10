dab0a8ec2f4a954b98728f4dd00e615ee3a386a9
skaffo <matteo.scaffidi@gmail.com>
2023-07-10 13:15:01 +0200

    Preparing slideshow from detail

--- src/pages/Mobile/Detail/Detail.tsx
+++ src/pages/Mobile/Detail/Detail.tsx
@@ -7,16 +7,30 @@ import { ReactComponent as Bell } from '../../../assets/bell.svg'
 import { ReactComponent as Castello } from '../../../assets/castelli.svg'
 import { ReactComponent as Camera } from '../../../assets/camera.svg'
 import { ReactComponent as CameraWhite } from '../../../assets/camera-white.svg'
+import { ReactComponent as CloseWhite } from '../../../assets/close-white.svg'
 import { useTranslation } from 'react-i18next'
 import { Swiper, SwiperSlide } from 'swiper/react'
+import { type Swiper as SwiperRef } from 'swiper'
 import 'swiper/css'
-import 'swiper/css/pagination'
+import { useRef, useState } from 'react'
+import classNames from 'classnames'
 
 export default function Detail() {
   const { slug } = useParams()
   const monument = useMonument(parseSmartSlug(slug!))
+  const [showAllImages, setShowAllImages] = useState(false)
+  const [slideShowActive, setSlideShowActive] = useState(0)
   const firstThreeImages = monument?.pictures.slice(0, 3)
+  const [slideActive, setSlideActive] = useState(0)
   const { t } = useTranslation()
+  const inputFileRef = useRef<HTMLInputElement>(null)
+  const swiperRef = useRef<SwiperRef>()
+  const swiperSlideShowRef = useRef<SwiperRef>()
+  const picturesById =
+    monument?.pictures.reduce((acc, picture) => {
+      acc[picture.id] = picture
+      return acc
+    }, {} as Record<string, (typeof monument.pictures)[0]>) ?? {}
   return (
     <Layout>
       <div className={styles.DettaglioContainer}>
@@ -29,11 +43,23 @@ export default function Detail() {
               onPaginationShow={() => {
                 console.log('Pagination show')
               }}
-
-              // modules={[Pagination]}
+              onSwiper={(swiper) => {
+                swiperRef.current = swiper
+              }}
+              onSlideChange={(swiper) => {
+                setSlideActive(swiper.activeIndex)
+              }}
+              className={styles.Swiper}
             >
               {firstThreeImages.map((picture) => (
-                <SwiperSlide key={picture.id}>
+                <SwiperSlide
+                  onClick={() => {
+                    console.log('ciao')
+                    setShowAllImages(true)
+                    setSlideShowActive(picture.id)
+                  }}
+                  key={picture.id}
+                >
                   <div
                     className={styles.BlockImage}
                     style={{
@@ -44,6 +70,20 @@ export default function Detail() {
               ))}
             </Swiper>
           )}
+          <div className={styles.PaginationImagesDots}>
+            {firstThreeImages.map((_, i) => (
+              <div
+                key={i}
+                className={classNames(styles.PaginationDot, {
+                  [styles.PaginationDotActive]:
+                    swiperRef.current?.activeIndex === i,
+                })}
+                onClick={() => {
+                  swiperRef.current?.slideTo(i)
+                }}
+              ></div>
+            ))}
+          </div>
           <div className={styles.CardInfoMonument}>
             <div className="d-flex justify-content-between w-100">
               <div className="d-flex">
@@ -81,11 +121,54 @@ export default function Detail() {
           </div>
         </div>
         <div className={styles.FixedButtonUpload}>
-          <button className={styles.ButtonUpload}>
+          <button
+            onClick={() => {
+              inputFileRef.current?.click()
+            }}
+            className={styles.ButtonUpload}
+          >
             <CameraWhite />
           </button>
+          <input
+            ref={inputFileRef}
+            type="file"
+            className="d-none"
+            accept="image/*"
+          />
         </div>
       </div>
+      {showAllImages && (
+        <div className={styles.SlideShow}>
+          <Swiper
+            className="h-100"
+            onSwiper={(swiper) => {
+              swiperRef.current = swiper
+            }}
+            onSlideChange={(swiper) => {
+                setSlideShowActive(swiper.activeIndex)
+            }}
+            
+          >
+            {monument?.pictures.map((picture) => (
+              <SwiperSlide key={picture.id}>
+                <div
+                  className={styles.SlideShowImage}
+                  style={{
+                    backgroundImage: `url(${picture.image_url})`,
+                  }}
+                ></div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+          <div className={styles.SlideShowClose}>
+            <CloseWhite
+              onClick={() => {
+                setShowAllImages(false)
+              }}
+            />
+          </div>
+        </div>
+      )}
     </Layout>
   )
 }
