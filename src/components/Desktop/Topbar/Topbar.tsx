6a101071647d285eab7d117ff86c09abac6603fa
skaffo <matteo.scaffidi@gmail.com>
2023-07-17 11:00:01 +0200

    Preparing wlm layout desktop

--- src/components/Desktop/Topbar/Topbar.tsx
+++ src/components/Desktop/Topbar/Topbar.tsx
@@ -1,9 +1,21 @@
 import styles from './Topbar.module.css'
+import { ReactComponent as Logo } from '../../../assets/wiki-logo-mobile.svg'
+import { ReactComponent as Bell } from '../../../assets/bell-primary.svg'
+import { useTranslation } from 'react-i18next'
 
 export default function Topbar() {
+  const { t } = useTranslation()
   return (
     <div className={styles.Topbar}>
-
+      <div className={styles.WikiLoveMonuments}>
+        <Logo /> {t('wiki_loves_monuments')}
+      </div>
+      <div>
+        <div className={styles.ButtonConcorso}>
+            <Bell /> {t('concorso_wiki_loves_monuments_1_30_settembre')}
+        </div>
+      </div>
+      <div />
     </div>
   )
 }
