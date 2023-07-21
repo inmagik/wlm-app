8733226e41e218e346fc7154da0274864531413c
skaffo <matteo.scaffidi@gmail.com>
2023-07-21 14:01:33 +0200

    Const for deploy

--- src/const.ts
+++ src/const.ts
@@ -1,4 +1,4 @@
 export const LANGS = ['it','en']
 
-export const API_URL = "http://192.168.1.109:5173/api/app"
-export const API_URL_SECOND = "http://192.168.1.109:5173/api"
+export const API_URL = "https://wlm.inmagik.com/api/app"
+export const API_URL_SECOND = "https://wlm.inmagik.com/api"
