cea31c35265f8882d661c989c19a61a660bdcaf5
skaffo <matteo.scaffidi@gmail.com>
2023-08-02 14:40:51 +0200

    Working on errors upload

--- src/const.ts
+++ src/const.ts
@@ -2,9 +2,9 @@ export const LANGS = ['it', 'en']
 
 export const API_URL = import.meta.env.PROD
   ? 'https://wlm.inmagik.com/api/app'
-  : 'https://wlm.inmagik.com/api/app'
+  : 'http://localhost:8000/api/app'
 export const API_URL_SECOND = import.meta.env.PROD
   ? 'https://wlm.inmagik.com/api'
-  : 'https://wlm.inmagik.com/api'
+  : 'http://localhost:8000/api'
 
 export const URL_WIKI = 'https://wikitest.inmagik.com/index.php'
