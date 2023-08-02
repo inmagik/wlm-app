f9cf2ff211ca547e6ca8bd5815abbcac6f6227c0
skaffo <matteo.scaffidi@gmail.com>
2023-08-02 09:47:40 +0200

    Working on profile

--- src/components/PersonalImages/PersonalImages.tsx
+++ src/components/PersonalImages/PersonalImages.tsx
@@ -4,16 +4,20 @@ import { useAuthUser } from 'use-eazy-auth'
 import { personalImages } from '../../hooks/monuments'
 import { UploadedImage } from '../../types'
 import { chunk } from 'lodash'
+import { useTranslation } from 'react-i18next'
 
 export function PersonalImages({ rows, cols }: { rows: number; cols: number }) {
   const { token } = useAuthUser()
   const [images, setImages] = useState<UploadedImage[]>([])
+  const { t } = useTranslation()
 
   useEffect(() => {
     personalImages(token).then((res) => {
       const pics = res.data.slice(0, rows * cols)
       if (pics.length < rows * cols) {
-        const emptyPics = new Array(rows * cols - pics.length).fill({ fake: true })
+        const emptyPics = new Array(rows * cols - pics.length).fill({
+          fake: true,
+        })
         setImages([...pics, ...emptyPics])
       } else {
         setImages(pics)
@@ -22,31 +26,32 @@ export function PersonalImages({ rows, cols }: { rows: number; cols: number }) {
   }, [token])
 
   return (
-    <div
-      className={styles.row}
-      style={{
-        gridTemplateColumns: `repeat(${cols}, 1fr)`,
-        gridTemplateRows: `repeat(${rows}, 1fr)`,
-      }}
-    >
-      {images.map((image) => {
-        console.log(image)
-        return (
-          <div>
-            {!image.fake && (
-              <img
-                src={image.imageinfo[0].url}
-                width={70}
-                height={70}
-                className={styles.image}
-              />
-            )}
-            {image.fake && (
-              <div className={styles.fakeImage} />
-            )}
-          </div>
-        )
-      })}
+    <div className={styles.container}>
+      <div className={styles.TitleLeTueFoto}>{t('le_tue_foto_caricate')}</div>
+      <div
+        className={styles.row}
+        style={{
+          gridTemplateColumns: `repeat(${cols}, 1fr)`,
+          gridTemplateRows: `repeat(${rows}, 1fr)`,
+        }}
+      >
+        {images.map((image) => {
+          console.log(image)
+          return (
+            <div>
+              {!image.fake && (
+                <img
+                  src={image.imageinfo[0].url}
+                  width={70}
+                  height={70}
+                  className={styles.image}
+                />
+              )}
+              {image.fake && <div className={styles.fakeImage} />}
+            </div>
+          )
+        })}
+      </div>
     </div>
   )
 }
