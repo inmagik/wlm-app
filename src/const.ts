b36c9aeaf09a4c0982f103f5db4fd1e33b26a764
bianchimro <bianchimro@gmail.com>
2023-08-25 11:04:42 +0200

    const for deployment

--- src/const.ts
+++ src/const.ts
@@ -1,10 +1,13 @@
 export const LANGS = ['it', 'en']
 
+const currentDomain = window.location.host
+export const IS_WIKI_DOMAIN = currentDomain.includes('wlm-it-app.wmcloud.org')
+
 export const API_URL = import.meta.env.PROD
-  ? 'https://wlm.inmagik.com/api/app'
+  ? IS_WIKI_DOMAIN ? 'https://wlm-it-visual.wmcloud.org/api/app' : 'https://wlm.inmagik.com/api/app'
   : 'http://localhost:8000/api/app'
 export const API_URL_SECOND = import.meta.env.PROD
-  ? 'https://wlm.inmagik.com/api'
+  ? IS_WIKI_DOMAIN ? 'https://wlm-it-visual.wmcloud.org/api' :'https://wlm.inmagik.com/api'
   : 'http://localhost:8000/api'
 
 export const URL_WIKI = 'https://wikitest.inmagik.com/index.php'
