4b9e5dde5fc26d0d9afee588089c2a9ff1d5e279
skaffo <matteo.scaffidi@gmail.com>
2023-08-29 10:42:20 +0200

    Removing logic default filter no contest

--- src/context/TopContext.tsx
+++ src/context/TopContext.tsx
@@ -10,20 +10,6 @@ interface ContextTop {
   //setGeoPermission?: (geoPermission: string) => void
 }
 
-const getFilters = (params: URLSearchParams) => ({
-  in_contest: params.get('in_contest') ?? 'true',
-  search: params.get('search') ?? '',
-  municipality: params.get('municipality') ?? '',
-  ordering: params.get('ordering') ?? '',
-  only_without_pictures: params.get('only_without_pictures') ?? '',
-  category: params.get('category') ?? '',
-  user_lat: params.get('user_lat') ?? '',
-  user_lon: params.get('user_lon') ?? '',
-  monument_lon: Number(params.get('monument_lon')) ?? '',
-  monument_lat: Number(params.get('monument_lat')) ?? '',
-  monument_id: Number(params.get('monument_id')) ?? '',
-})
-
 const TopStateContext = createContext<ContextTop>({
   activeContests: [],
 })
@@ -35,17 +21,6 @@ export function TopContextProvider({
 }) {
 
   const { data: contests } = useActiveContests()
-
-  const { filters, setFilters } = useQsFilters(getFilters)
-  
-  useEffect(() => {
-    if(contests && contests.length === 0){
-      setFilters({
-        ...filters,
-        in_contest: ''
-      })
-    }
-  },[contests])
 
   //geo perms
   const [geoPermission, setGeoPermission] = useState<string>('prompt')
