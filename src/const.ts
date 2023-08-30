89918395c1af1725141b5c52a770eb66215dbd62
skaffo <matteo.scaffidi@gmail.com>
2023-08-30 16:18:36 +0200

    Fixing const for profile url

--- src/const.ts
+++ src/const.ts
@@ -1,13 +1,19 @@
 export const LANGS = ['it', 'en']
 
 const currentDomain = window.location.host
-export const IS_WIKI_DOMAIN = currentDomain.includes('wlm-it-app.wmcloud.org')
+export const IS_WIKI_DOMAIN = currentDomain.includes('wlm-it-app.wmcloud.org') || currentDomain.includes('app.wikilovesmonuments.it')
 
 export const API_URL = import.meta.env.PROD
-  ? IS_WIKI_DOMAIN ? 'https://wlm-it-visual.wmcloud.org/api/app' : 'https://wlm.inmagik.com/api/app'
+  ? IS_WIKI_DOMAIN
+    ? 'https://wlm-it-visual.wmcloud.org/api/app'
+    : 'https://wlm.inmagik.com/api/app'
   : 'http://localhost:8000/api/app'
 export const API_URL_SECOND = import.meta.env.PROD
-  ? IS_WIKI_DOMAIN ? 'https://wlm-it-visual.wmcloud.org/api' :'https://wlm.inmagik.com/api'
+  ? IS_WIKI_DOMAIN
+    ? 'https://wlm-it-visual.wmcloud.org/api'
+    : 'https://wlm.inmagik.com/api'
   : 'http://localhost:8000/api'
 
-export const URL_WIKI = 'https://wikitest.inmagik.com/index.php'
+export const URL_WIKI = IS_WIKI_DOMAIN
+  ? 'https://commons.wikimedia.org/wiki'
+  : 'https://wikitest.inmagik.com/index.php'
