2b77103948b026762d696ee38c3fe67dcde4ce4e
skaffo <matteo.scaffidi@gmail.com>
2023-09-04 11:09:13 +0200

    Fixing virtual slideshow slider

--- src/components/Mobile/SlideShow/SlideShow.tsx
+++ src/components/Mobile/SlideShow/SlideShow.tsx
@@ -10,7 +10,7 @@ import styles from './SlideShow.module.css'
 import IconMonument from '../../IconMonument'
 import { useTranslation } from 'react-i18next'
 import dayjs from 'dayjs'
-import { Navigation } from 'swiper'
+import { Navigation, Virtual } from 'swiper'
 import classNames from 'classnames'
 import { isBrowserMobile } from '../../../utils'
 
@@ -68,7 +68,8 @@ export default function SlideShow({
         onSwiper={(swiper) => {
           swiperSlideShowRef.current = swiper
         }}
-        modules={[Navigation]}
+        virtual
+        modules={[Navigation, Virtual]}
         navigation
         onSlideChange={(swiper) => {
           setSlideShowActive(swiper.activeIndex)
